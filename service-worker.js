20b49cc8dfdb0ae056d47cbf49daf6bcaa7ff3c2
Kandiak Oleg <kandio1604@gmail.com>
2021-07-28 13:22:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.f7fd3a068cc7764f930746731fa2a3e4.js"
 );
 
 self.addEventListener('message', (event) => {
